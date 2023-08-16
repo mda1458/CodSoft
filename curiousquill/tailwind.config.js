8dfb12b22339687ec14ded493f650071d9572992
Muhammad Danish Azeem <88603558+mda1458@users.noreply.github.com>
2023-08-16 06:41:15 +0500

    shifted project to next js due to dynamic routing

--- curiousquill/tailwind.config.js
+++ curiousquill/tailwind.config.js
@@ -1,20 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
     extend: {
-      fontSize: {
-        "md": "1rem",
-        "lg": "1.25rem",
-        "xl": "2rem",
-        "2xl": "2.5rem",
-        "3xl": "3rem",
-        "4xl": "3.5rem",
-        "5xl": "4rem",
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
-    }
+    },
   },
   plugins: [],
-};
-
-
+}
