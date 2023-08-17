1f1b22d726c0701784594a94ec126d1cdf5a7d88
Muhammad Danish Azeem <88603558+mda1458@users.noreply.github.com>
2023-08-17 11:26:50 +0500

    some changes

--- curiousquill/src/components/Main/Blogs/Blogs.jsx
+++ curiousquill/src/components/Main/Blogs/Blogs.jsx
@@ -1,4 +1,4 @@
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { firestore } from "../../../firebase";
 import { useEffect, useState } from "react";
 import Loader from "../../Loader/Loader";
@@ -43,23 +43,6 @@ const Blogs = () => {
       <div className="flex items-center justify-center flex-wrap mt-10">
         {loading? <Loader />
         : data.map((blog) => (
-          // <div className="w-[30%] p-4" key={blog.id}>
-          //   <img src={blog.img} alt="" className="w-20"/>
-          //   <div className="shadow-lg rounded-lg px-4 py-6">
-          //     <h2 className="text-lg font-bold text-gray-800">{blog.title}</h2>
-          //     <p className="mt-2 text-gray-600">
-          //       {blog.desc.slice(0, 200) + "..."}
-          //     </p>
-          //     <div className="flex items-center justify-between mt-4">
-          //       <Link
-          //         to={`/blog/${blog.id}`}
-          //         className="text-blue-500 hover:underline"
-          //       >
-          //         Read more
-          //       </Link>
-          //     </div>
-          //   </div>
-          // </div>
           <BlogCard blog={blog} key={blog.id}/>
         ))}
       </div>
