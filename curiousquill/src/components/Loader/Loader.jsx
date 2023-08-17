5e6bd10acfc05cfb0fd9acb1f01bd7e6ae1ee426
Muhammad Danish Azeem <88603558+mda1458@users.noreply.github.com>
2023-08-17 10:23:03 +0500

    setup the loader

---
+++ curiousquill/src/components/Loader/Loader.jsx
@@ -0,0 +1,14 @@
+import './loader.css'
+
+const Loader = () => {
+  return (
+    <div className="containers">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  );
+}
+
+export default Loader
